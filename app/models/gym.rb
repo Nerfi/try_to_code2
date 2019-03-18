fc613cb95dfbf70eae41aeb0e97a0745186f7e79
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-18 23:02:52 +0100

    working

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,4 +1,11 @@
 class Gym < ApplicationRecord
   belongs_to :user
    mount_uploader :photo, PhotoUploader
+
+  validates :name, presence: true , uniqueness: true
+  validates :description, presence: true
+  validates :description, length: { minimum: 15 }
+  validates :pricing, presence: true
+  validates :address , presence: true
+  #para poder añadir en nuestro model
 end
